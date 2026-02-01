ad642e1a24e77e420bc252269e7b5348b5c9b996
Janani <janvarsh2128@gmail.com>
2026-02-01 13:51:49 +0530

    auth implemented

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <App />
+    </ClerkProvider>
   </StrictMode>
 );
